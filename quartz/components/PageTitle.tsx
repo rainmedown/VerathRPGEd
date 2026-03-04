567b8774bc6367149ec21404616028bf9be4aa3d
Gustavo Tito <gustavotito@MacBook-Air-de-Gustavo.local>
2026-03-04 19:19:39 -0300

    add verath logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,10 +7,12 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
-  )
+  <div class={classNames(displayClass, "page-title")}>
+    <a href={baseDir}>
+      <img src="/static/verath-logo.png" alt="Verath" style="height:60px;" />
+    </a>
+  </div>
+)
 }
 
 PageTitle.css = `
